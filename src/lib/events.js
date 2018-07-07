5262dc31a89df3cee694e23a4af100697ae7de89
Jesse Onolememen <jesseonolememen@Jesses-MacBook-Pro.local>
2018-07-07 22:23:40 +0100

    Updated events

--- src/lib/events.js
+++ src/lib/events.js
@@ -16,6 +16,7 @@ export function watch() {
   
   // Binlog must be started, optionally pass in filters
   zongji.start({
+    serverId: 1,
     includeEvents: ['tablemap', 'writerows', 'updaterows', 'deleterows']
   });
 
