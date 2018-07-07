48dcc10beb058791a42b107d56ae6647c7e69267
Jesse Onolememen <jesseonolememen@Jesses-MacBook-Pro.local>
2018-07-07 22:17:16 +0100

    Updated events

--- src/lib/events.js
+++ src/lib/events.js
@@ -8,35 +8,24 @@ export function watch() {
     host: 'localhost',
     user: 'root',
     password: '',
-    database : 'easyjet',
-  }
+  };
 
-  const con = mysql.createConnection(config)
-
-  con.connect(function(err) {
-    if (err) {
-      logger.debug(err)
-    } else {
-      logger.debug('done')
-    }
-  })
-  
-  // const EventWatcher = MySQLEvents(config);
-  // EventWatcher.add(
-  //   `${env.MYSQL_DB_NAME}.data.notice.value`,
-  //   (oldRow, newRow, event) => {
-  //     if (oldRow === null) {
-  //       logger.debug('Added new row to table');
-  //     }
+  const EventWatcher = MySQLEvents(config);
+  EventWatcher.add(
+    `easyjet.data.notice.value`,
+    (oldRow, newRow, event) => {
+      if (oldRow === null) {
+        logger.debug('Added new row to table');
+      }
    
-  //     if (newRow === null) {
-  //       logger.debug('Deleted a row from table');
-  //     }
+      if (newRow === null) {
+        logger.debug('Deleted a row from table');
+      }
    
-  //     if (oldRow !== null && newRow !== null) {
-  //       logger.debug('Updated a row from table');
-  //     }
-  //   },
-  // )
+      if (oldRow !== null && newRow !== null) {
+        logger.debug('Updated a row from table');
+      }
+    },
+  )
 
 }
